016e43fb45f0473e7f769182c14df3b4942c7308
beatrizfagundesalves <beatriz.fagundes.alves@escola.pr.gov.br>
2026-06-10 18:38:19 +0000

    TERMINEI O PROJETO

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const botao = document.getElementById("saibaMaisBtn");
-const textoExtra = document.getElementById("textoExtra");
+// Seleção dos elementos do HTML
+const btnSaibaMais = document.getElementById('btn-saiba-mais');
+const modalTexto = document.getElementById('modal-texto');
+const btnFechar = document.getElementById('btn-fechar');
 
-botao.addEventListener("click", () => {
-    if (textoExtra.style.display === "block") {
-        textoExtra.style.display = "none";
-        botao.textContent = "Saiba Mais";
-    } else {
-        textoExtra.style.display = "block";
-        botao.textContent = "Mostrar Menos";
+// Abrir o texto explicativo ao clicar no botão
+btnSaibaMais.addEventListener('click', () => {
+    modalTexto.classList.remove('escondido');
+});
+
+// Fechar o texto explicativo ao clicar no X
+btnFechar.addEventListener('click', () => {
+    modalTexto.classList.add('escondido');
+});
+
+// Fechar o texto se o usuário clicar fora da caixinha branca
+window.addEventListener('click', (event) => {
+    if (event.target === modalTexto) {
+        modalTexto.classList.add('escondido');
     }
 });
+
